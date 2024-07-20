7328fd096bbc7d6a7a03d97bd7c7563f986cd326
Elshater6498 <elshaterm247@gmail.com>
2024-07-20 15:29:50 +0300

    breif the code

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 
 const Clock = () => {
-  let time = new Date().toLocaleTimeString();
-  const [currentTime, setCurrentTime] = useState(time);
+  const [currentTime, setCurrentTime] = useState(
+    new Date().toLocaleTimeString()
+  );
 
   const updateTime = () => {
-    let time = new Date().toLocaleTimeString();
-    setCurrentTime(time);
+    setCurrentTime(new Date().toLocaleTimeString());
   };
   setInterval(updateTime, 1000);
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-center text-2xl border-2 border-[#deb887] rounded-md py-2 w-[250px] mx-auto bg-[#fff8e3] shadow-[0px_5px_15px_rgba(0,0,0,0.35)] text-[#c4a661]">{currentTime}</h1>
+      <h1 className="text-center text-2xl border-2 border-[#deb887] rounded-md py-2 w-[250px] mx-auto bg-[#fff8e3] shadow-[0px_5px_15px_rgba(0,0,0,0.35)] text-[#c4a661]">
+        {currentTime}
+      </h1>
     </div>
   );
 };
